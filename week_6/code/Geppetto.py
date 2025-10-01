ddbce8020895c96367d7e9db390f02c414391e07
Oscar <orss4123@gmail.com>
2025-10-01 09:25:54 -0400

    slow

--- week_6/code/Geppetto.py
+++ week_6/code/Geppetto.py
@@ -1,8 +1,12 @@
 from itertools import combinations
 
 n, m = [int(x) for x in input().split()]
+comb = [()]
+for i in range(1, n + 1):
+    comb += list(combinations(range(1, n + 1), i))
 
-comb = combinations(range(1, n + 1), 2)
+for _ in range(m):
+    prohibition = tuple([int(x) for x in input().split()])
+    comb = list(filter(lambda c: not all(x in c for x in prohibition), comb))
 
-for i in comb:
-    print(i)
+print(len(comb))
