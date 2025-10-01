c7e821f41bffec8b4bb2598ea1b95269de708a93
Oscar <orss4123@gmail.com>
2025-10-01 09:34:10 -0400

    using bit masking

--- week_6/code/Geppetto.py
+++ week_6/code/Geppetto.py
@@ -1,12 +1,13 @@
-from itertools import combinations
-
-n, m = [int(x) for x in input().split()]
-comb = [()]
-for i in range(1, n + 1):
-    comb += list(combinations(range(1, n + 1), i))
-
+n, m = map(int, input().split())
+prohibitions = []
 for _ in range(m):
-    prohibition = tuple([int(x) for x in input().split()])
-    comb = list(filter(lambda c: not all(x in c for x in prohibition), comb))
+    p = list(map(int, input().split()))
+    mask = sum(1 << (x-1) for x in p)
+    prohibitions.append(mask)
 
-print(len(comb))
+pizzas = 0
+for s in range(1 << n):
+    if all((s & p) != p for p in prohibitions):
+        pizzas += 1
+
+print(pizzas)
